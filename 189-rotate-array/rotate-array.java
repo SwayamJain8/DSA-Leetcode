b463506c61e7b9e1efe9cd272f699e88b962c3c8
Swayam <160944160+SwayamJain8@users.noreply.github.com>
2025-02-14 16:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (34.98%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,15 +2,22 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        helper(0, n, nums);
-        helper(0, k, nums);
-        helper(k, n, nums);
+        helper(0, n-1, nums);
+        helper(0, k-1, nums);
+        helper(k, n-1, nums);
     }
     public void helper(int start, int end, int nums[]) {
-            for(int i = start; i< (start+end)/2; i++) {
-                int temp = nums[i];
-                nums[i] = nums[(start+end)-i-1];
-                nums[(start+end)-i-1] = temp;
-            }
+        // for(int i = start; i< (start+end)/2; i++) {
+        //     int temp = nums[i];
+        //     nums[i] = nums[(start+end)-i-1];
+        //     nums[(start+end)-i-1] = temp;
+        // }
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp; 
+            start++;
+            end--;
+        }
     }
 }
